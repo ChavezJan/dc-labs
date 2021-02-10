77edf75f361b205873029cd6171e98dfbb5b0b99
Jorge <chavejoa@gmail.com>
2021-02-10 12:51:47 -0600

    Maps and Slices Complete
    Signed-off-by: Jorge <chavejoa@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+
+	Rmap := map[string]int{}
+	for _, word := range strings.Fields(s) {
+		Rmap[word]++
+	}
+	return Rmap
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,23 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+
+	var image [][]uint8
+
+	for i := 0; i < dy; i++ {
+		var row []uint8
+		for j := 0; j < dx; j++ {
+			pixel := uint8((i + j) / 2)
+			row = append(row, pixel)
+		}
+		image = append(image, row)
+	}
+	return image
+
+}
+
 func main() {
+	pic.Show(Pic)
 }
