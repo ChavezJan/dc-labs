d92dfb6077a98a18d1d9220853258031539f2800
Jorge <chavejoa@gmail.com>
2021-03-25 18:23:23 -0600

    Aws s3 V0.1

--- labs/aws-s3-explorer/awsS3Client.go
+++ labs/aws-s3-explorer/awsS3Client.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/http"
+)
+
 func main() {
+
+	var proxy = flag.String("proxy", "localhost:8000", "Proxy url.")
+	var bucketName = flag.String("bucket", "", "S3 bucket name.")
+	var directory = flag.String("directory", "", "Directory name.")
+	flag.Parse()
+
+	// error if bucket parameter is missing, the server cannot process anything
+	if *bucketName == "" {
+		fmt.Println("ERROR - Missing parameters.")
+		return
+	}
+
+	// request to server the bucket and directory through a url
+	request := fmt.Sprintf("http://%v/example?bucket=%v&dir=%v", *proxy, *bucketName, *directory)
+	resp, err := http.Get(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// prints the response from server
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan(); i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
--- labs/aws-s3-explorer/awsS3Explorer.go
+++ labs/aws-s3-explorer/awsS3Explorer.go
@@ -1,4 +1,179 @@
+// server that serves the bucket and directory details in JSON format
 package main
 
+import (
+	"encoding/json"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+// key objects from xml
+type Content struct {
+	Key string `xml:"Key"`
+}
+
+// xml data struct receiver
+type XMLResult struct {
+	XMLName  xml.Name  `xml:"ListBucketResult"`
+	Name     string    `xml:"Name"`
+	Contents []Content `xml:"Contents"`
+}
+
+// result struct 1
+type BucketDetails struct {
+	BucketName       string
+	ObjectsCount     int
+	DirectoriesCount int
+	Extensions       map[string]int
+}
+
+// result struct 2
+type DirDetails struct {
+	BucketName       string
+	DirectoryName    string
+	ObjectsCount     int
+	DirectoriesCount int
+	Extensions       map[string]int
+}
+
+func responseHandler(res http.ResponseWriter, req *http.Request) {
+
+	limit := 3 // the explorer cannot check further than 3 levels on the directory tree
+	extensions := make(map[string]int)
+	directories := make(map[string]bool)
+	objects := make(map[string]bool)
+
+	bucketName := req.FormValue("bucket")
+	reqDir := req.FormValue("dir")
+	url := fmt.Sprintf("https://%v.s3.amazonaws.com", bucketName)
+
+	// get the url response from S3
+	resp, gerr := http.Get(url)
+	if gerr != nil {
+		println("ERROR - Get S3 connection error.")
+		io.WriteString(res, "ERROR - Get S3 connection error.")
+		return
+	}
+	defer resp.Body.Close()
+
+	data, rerr := ioutil.ReadAll(resp.Body)
+	if rerr != nil {
+		println("ERROR - Http response reading error.")
+		io.WriteString(res, "ERROR - Http response reading error.")
+		return
+	}
+
+	// decode xml into struct
+	var xmlResult XMLResult
+	xerr := xml.Unmarshal(data, &xmlResult)
+	if xerr != nil {
+		println("ERROR - Permission denied bucket.")
+		io.WriteString(res, "ERROR - Permission denied bucket.\n")
+		return
+	}
+
+	// read and process struct content
+	for _, c := range xmlResult.Contents {
+		objKey := c.Key
+		dir := fmt.Sprintf("%v/", reqDir)
+
+		// conditions to filter directory cases
+		if reqDir != "" && !strings.HasPrefix(objKey, dir) {
+			continue
+		}
+
+		if reqDir != "" && strings.HasPrefix(objKey, dir) {
+			objKey = strings.Replace(objKey, dir, "", 1)
+			if objKey == "" {
+				// remaining directory is the request directory itself
+				continue
+			}
+		}
+
+		// if there is more than 3 directories in the tree, discard the key
+		if strings.Count(objKey, "/") > limit {
+			continue
+		}
+
+		// process the key string
+		if strings.HasSuffix(objKey, "/") {
+			if !directories[objKey] {
+				directories[objKey] = true
+			}
+		}
+
+		if strings.Contains(objKey, ".") {
+			_, exists := objects[objKey]
+			if !exists {
+				objects[objKey] = true
+			}
+
+			ext := strings.Split(objKey, ".")
+			_, exists = extensions[ext[len(ext)-1]]
+			if !exists {
+				extensions[ext[len(ext)-1]] = 1
+			}
+			if exists {
+				extensions[ext[len(ext)-1]] += 1
+			}
+
+		}
+	}
+
+	// create result struct with final dictionaries and transform it to JSON
+	if reqDir == "" {
+		bd := BucketDetails{
+			BucketName:       xmlResult.Name,
+			ObjectsCount:     len(objects),
+			DirectoriesCount: len(directories),
+			Extensions:       extensions,
+		}
+
+		out, err := json.MarshalIndent(bd, "", "\t")
+		if err != nil {
+			panic(err)
+		}
+		res.Header().Set("Content-Type", "application/json")
+		res.Write(out)
+	}
+	if reqDir != "" {
+		dd := DirDetails{
+			BucketName:       xmlResult.Name,
+			DirectoryName:    reqDir,
+			ObjectsCount:     len(objects),
+			DirectoriesCount: len(directories),
+			Extensions:       extensions,
+		}
+
+		out, err := json.MarshalIndent(dd, "", "\t")
+		if err != nil {
+			panic(err)
+		}
+		res.Header().Set("Content-Type", "application/json")
+		res.Write(out)
+	}
+}
 func main() {
+
+	var port = flag.Int("port", 9000, "Port number.")
+	flag.Parse()
+	fmt.Printf("===== Service on port: %v =====\n", *port) // port is a pointer
+
+	socket := fmt.Sprintf("localhost:%v", *port)
+
+	// get the url from cliente request
+	http.HandleFunc("/example", func(res http.ResponseWriter, req *http.Request) {
+		responseHandler(res, req)
+	})
+
+	err := http.ListenAndServe(socket, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
